bf68a95412dd4f1b71a4e4c52cf438e4e53e1024
Quentin Widlocher <qwidlocher@gmail.com>
2019-12-23 15:38:11 +0100

    :truck: Refine folder architecture

---
+++ src/views/Home/Home.ts
@@ -0,0 +1,8 @@
+import { Component, Vue } from "vue-property-decorator";
+import { firebaseService } from "@/services/firebase";
+import router from '@/router/index';
+import { globalVariables } from "@/services/globalVariables";
+
+@Component
+export default class Home extends Vue {
+}
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="home"></div>    
+</template>
+<style lang="scss" src="./Home.scss" scoped></style>
+<script lang="ts" src="./Home.ts"></script>
