4817b7fb5a3cce6a60fe255cea0b1c233fb34415
Quentin Widlocher <qwidlocher@gmail.com>
2019-12-26 19:21:28 +0100

    :heavy_plus_sign: Add Vuex

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import "./registerServiceWorker";
 import vuetify from './plugins/vuetify';
 import i18n from './i18n'
+import store from './store'
 
 Vue.config.productionTip = false;
 
@@ -11,5 +12,6 @@ new Vue({
   router,
   vuetify,
   i18n,
+  store,
   render: h => h(App)
 }).$mount("#app");
