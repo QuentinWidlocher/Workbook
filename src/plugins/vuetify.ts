5898c32860502fc8ad8a1bad85fc15dc4a878a87
Quentin Widlocher <qwidlocher@gmail.com>
2020-01-16 11:35:53 +0100

    :heavy_plus_sign: Icons and font are offline

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,6 +2,13 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import fr from 'vuetify/src/locale/fr';
 import en from 'vuetify/src/locale/en';
+import '@mdi/font/css/materialdesignicons.min.css';
+import '../../public/fonts/Roboto/Roboto-Bold.ttf';
+import '../../public/fonts/Roboto/Roboto-BoldItalic.ttf';
+import '../../public/fonts/Roboto/Roboto-Light.ttf';
+import '../../public/fonts/Roboto/Roboto-LightItalic.ttf';
+import '../../public/fonts/Roboto/Roboto-Regular.ttf';
+import '../../public/fonts/Roboto/Roboto-Italic.ttf';
 
 Vue.use(Vuetify);
 
