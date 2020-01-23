0d0604db3c2bfb0e1b703bec74df7b001ef509ff
Quentin Widlocher <qwidlocher@gmail.com>
2020-01-23 22:27:01 +0100

    :lipstick: :bug: Fix toolbar too dark in light mode

--- src/components/Edition/Editor/Editor.ts
+++ src/components/Edition/Editor/Editor.ts
@@ -1,51 +1,30 @@
-import { Component, Vue, Prop, Watch } from "vue-property-decorator";
-import { Editor as TipTapEditor, EditorContent, EditorMenuBar } from "tiptap";
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import { Editor as TipTapEditor, EditorContent, EditorMenuBar } from 'tiptap';
 import {
-  Bold,
-  Italic,
-  Underline,
-  Code,
-  Blockquote,
-  CodeBlock,
-  Heading,
-  OrderedList,
-  BulletList,
-  ListItem,
-  TodoItem,
-  TodoList,
-  HorizontalRule,
-  Link,
-  Strike,
-  History,
-  HardBreak,
-  Image,
-  Table,
-  TableHeader,
-  TableCell,
-  TableRow,
-} from "tiptap-extensions";
-import { 
-  BoldAction,
-  ItalicAction,
-  UnderlineAction,
-  SpacerAction,
-  HeadingsAction,
-  LinkAction,
-  CodeAction,
-  StrikeAction,
-  QuoteAction,
-  RuleAction,
-  BulletListAction,
-  OrderedListAction,
-  TodoListAction,
-  ImageAction,
-  TableAction,
- } from "./Actions";
-
-@Component({
-  components: {
-    EditorContent,
-    EditorMenuBar,
+    Bold,
+    Italic,
+    Underline,
+    Code,
+    Blockquote,
+    CodeBlock,
+    Heading,
+    OrderedList,
+    BulletList,
+    ListItem,
+    TodoItem,
+    TodoList,
+    HorizontalRule,
+    Link,
+    Strike,
+    History,
+    HardBreak,
+    Image,
+    Table,
+    TableHeader,
+    TableCell,
+    TableRow,
+} from 'tiptap-extensions';
+import {
     BoldAction,
     ItalicAction,
     UnderlineAction,
@@ -61,75 +40,97 @@ import {
     TodoListAction,
     ImageAction,
     TableAction,
-  }
+} from './Actions';
+import { debug } from 'webpack';
+
+@Component({
+    components: {
+        EditorContent,
+        EditorMenuBar,
+        BoldAction,
+        ItalicAction,
+        UnderlineAction,
+        SpacerAction,
+        HeadingsAction,
+        LinkAction,
+        CodeAction,
+        StrikeAction,
+        QuoteAction,
+        RuleAction,
+        BulletListAction,
+        OrderedListAction,
+        TodoListAction,
+        ImageAction,
+        TableAction,
+    },
 })
 export default class Editor extends Vue {
-  @Prop({ default: "" }) value!: string;
+    @Prop({ default: '' }) value!: string;
 
-  private topRowButtons = [
-    "Bold",
-    "Italic",
-    "Underline",
-    "Spacer",
-    "Headings",
-    "Spacer",
-    "Link",
-    "Image",
-    "Spacer",
-    "BulletList",
-    "OrderedList",
-    "TodoList",
-    "Spacer",
-    "Table"
-  ];
-  private bottomRowButtons = ["Code", "Strike", "Quote", "Spacer", "Rule"];
+    private topRowButtons = [
+        'Bold',
+        'Italic',
+        'Underline',
+        'Spacer',
+        'Headings',
+        'Spacer',
+        'Link',
+        'Image',
+        'Spacer',
+        'BulletList',
+        'OrderedList',
+        'TodoList',
+        'Spacer',
+        'Table',
+    ];
+    private bottomRowButtons = ['Code', 'Strike', 'Quote', 'Spacer', 'Rule'];
 
-  private extendedToolbar: boolean = false;
+    private extendedToolbar: boolean = false;
 
-  private editor: TipTapEditor = new TipTapEditor({
-    content: this.value,
-    extensions: [
-      new Bold(),
-      new Italic(),
-      new Underline(),
-      new Code(),
-      new Blockquote(),
-      new CodeBlock(),
-      new Heading({ levels: [1, 2, 3] }),
-      new OrderedList(),
-      new BulletList(),
-      new ListItem(),
-      new TodoItem({nested: true}),
-      new TodoList(),
-      new Link(),
-      new Strike(),
-      new History(),
-      new HorizontalRule(),
-      new HardBreak(),
-      new Image(),
-      new Table({ resizable: true }),
-      new TableHeader(),
-      new TableCell(),
-      new TableRow(),
-    ],
-    onUpdate: (info: any) => {
-      this.onUpdate(info);
+    private editor: TipTapEditor = new TipTapEditor({
+        content: this.value,
+        extensions: [
+            new Bold(),
+            new Italic(),
+            new Underline(),
+            new Code(),
+            new Blockquote(),
+            new CodeBlock(),
+            new Heading({ levels: [1, 2, 3] }),
+            new OrderedList(),
+            new BulletList(),
+            new ListItem(),
+            new TodoItem({ nested: true }),
+            new TodoList(),
+            new Link(),
+            new Strike(),
+            new History(),
+            new HorizontalRule(),
+            new HardBreak(),
+            new Image(),
+            new Table({ resizable: true }),
+            new TableHeader(),
+            new TableCell(),
+            new TableRow(),
+        ],
+        onUpdate: (info: any) => {
+            this.onUpdate(info);
+        },
+    });
+
+    private emitAfterOnUpdate: boolean = false;
+
+    private onUpdate(info: any) {
+        this.emitAfterOnUpdate = true;
+        this.$emit('input', info.getHTML());
     }
-  });
 
-  private emitAfterOnUpdate: boolean = false;
-
-  private onUpdate(info: any) {
-    this.emitAfterOnUpdate = true;
-    this.$emit("input", info.getHTML());
-  }
-
-  @Watch("value")
-  private onDescriptionChanged() {
-    if (this.emitAfterOnUpdate) {
-      this.emitAfterOnUpdate = false;
-      return;
+    @Watch('value')
+    private onDescriptionChanged() {
+        if (this.emitAfterOnUpdate) {
+            this.emitAfterOnUpdate = false;
+            return;
+        }
+        this.editor.setContent(this.value);
     }
-    this.editor.setContent(this.value);
-  }
 }
--- src/components/Edition/Editor/Editor.vue
+++ src/components/Edition/Editor/Editor.vue
@@ -1,62 +1,53 @@
 <template>
-  <div id="Editor">
-    <v-card>
-      <EditorMenuBar
-        :editor="editor"
-        v-slot="{ commands, isActive, getMarkAttrs }"
-      >
-        <v-toolbar flat dense :class="{'grey lighten-4': $vuetify.dark, 'grey darken-4': !$vuetify.dark}">
-          <component
-            v-for="(button, i) in topRowButtons"
-            :key="`btn-${i}-${button}`"
-            :is="button + 'Action'"
-            :commands="commands"
-            :isActive="isActive"
-            :getMarkAttrs="getMarkAttrs"
-          />
-
-          <v-spacer />
-
-          <v-tooltip top>
-            <template v-slot:activator="{ on }">
-              <v-btn
-                icon
-                small
-                @click="extendedToolbar = !extendedToolbar"
-                v-on="on"
-              >
-                <v-icon
-                  >mdi-chevron-{{ extendedToolbar ? "up" : "down" }}</v-icon
+    <div id="Editor">
+        <v-card>
+            <EditorMenuBar :editor="editor" v-slot="{ commands, isActive, getMarkAttrs }">
+                <v-toolbar
+                    flat
+                    dense
+                    :class="{ 'grey lighten-4': !$vuetify.theme.dark, 'grey darken-2': $vuetify.theme.dark }"
                 >
-              </v-btn>
-            </template>
+                    <component
+                        v-for="(button, i) in topRowButtons"
+                        :key="`btn-${i}-${button}`"
+                        :is="button + 'Action'"
+                        :commands="commands"
+                        :isActive="isActive"
+                        :getMarkAttrs="getMarkAttrs"
+                    />
 
-            <span>{{
-              $t(
-                "edition.editor.actions.see" +
-                  (extendedToolbar ? "Less" : "More")
-              )
-            }}</span>
-          </v-tooltip>
+                    <v-spacer />
 
-          <template v-if="extendedToolbar" #extension>
-            <component
-              v-for="(button, i) in bottomRowButtons"
-              :key="`btn-${i}-${button}`"
-              :is="button + 'Action'"
-              :commands="commands"
-              :isActive="isActive"
-              :getMarkAttrs="getMarkAttrs"
-            />
-          </template>
-        </v-toolbar>
-      </EditorMenuBar>
+                    <v-tooltip top>
+                        <template v-slot:activator="{ on }">
+                            <v-btn icon small @click="extendedToolbar = !extendedToolbar" v-on="on">
+                                <v-icon>mdi-chevron-{{ extendedToolbar ? 'up' : 'down' }}</v-icon>
+                            </v-btn>
+                        </template>
 
-      <v-card-text class="v-card__text" :style="extendedToolbar ? '--extended: 2' : '--extended: 1'">
-        <EditorContent id="content" :editor="editor"></EditorContent>
-      </v-card-text>
-    </v-card>
-  </div>
+                        <span>{{
+                            $t('edition.editor.actions.see' + (extendedToolbar ? 'Less' : 'More'))
+                        }}</span>
+                    </v-tooltip>
+
+                    <template v-if="extendedToolbar" #extension>
+                        <component
+                            v-for="(button, i) in bottomRowButtons"
+                            :key="`btn-${i}-${button}`"
+                            :is="button + 'Action'"
+                            :commands="commands"
+                            :isActive="isActive"
+                            :getMarkAttrs="getMarkAttrs"
+                        />
+                    </template>
+                </v-toolbar>
+            </EditorMenuBar>
+
+            <v-card-text class="v-card__text" :style="extendedToolbar ? '--extended: 2' : '--extended: 1'">
+                <EditorContent id="content" :editor="editor"></EditorContent>
+            </v-card-text>
+        </v-card>
+    </div>
 </template>
 <style lang="scss" src="./Editor.scss"></style>
 <script lang="ts" src="./Editor.ts"></script>
